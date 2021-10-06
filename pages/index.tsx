6664983c526fa13925c317faa47c81853bd4aa0b
NicholasNeto <nicholasneto@outlook.com>
2021-10-05 21:14:55 -0300

    created a form for find same prodect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,37 @@
+import { FormEvent, useState } from "react"
+import { SearchResult } from '../components/SearchResult'
 
 export default function Home() {
+  const [search, setSearch] = useState('')
+  const [results, setResults] = useState([])
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`)
+    const data = await response.json()
+    setResults(data)
+  }
+
   return (
     <div>
-      Hello World
+      <h1>Search</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+
+      <SearchResult results={results} />
+
     </div>
   )
 }
