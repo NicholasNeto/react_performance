9b89724b72196dfdaf76fe38021e95e947eda6c6
NicholasNeto <nicholasneto@outlook.com>
2021-10-08 22:08:53 -0300

    add useMemo in project in calc totalPrice

--- components/SearchResult.tsx
+++ components/SearchResult.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { ProductItem } from "./ProductItem"
 
 
@@ -10,11 +11,35 @@ interface SearchResultProps {
 }
 
 export function SearchResult({ results }: SearchResultProps) {
+    const totalPrice = useMemo(() => {
+        return results.reduce((total, product) => {
+            return total + product.price;
+        }, 0)
+    }, [results])
+
     return (
         <div>
+            <h2>{totalPrice}</h2>
+
             {results.map(product => {
                 return <ProductItem product={product} />
             })}
         </div>
     )
 }
+
+
+/*
+Memo 
+1. Pure Functional Components
+2. Renders too often 
+3. Re-renders with same props
+4. Medium to big size
+*/
+
+/*
+useMemo
+
+1. Calculos pesados
+2. Igualdade referencial (quando a gente repassa aquela informação a um componente filho)
+*/
